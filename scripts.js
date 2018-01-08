91628f6eb90157861962f2223fc3d88f8d9c99fe
Robert Frazier <robert.v.frazier@gmail.com>
2018-01-08 18:46:49 -0500

    Submenu How It Works now toggles its list items.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 "use strict";
 
-$(document).ready(function() {
+function main (){
+    toggleMenu ();
+    toggleCourses();
+    toggleEducation();
+};
+
+function toggleMenu (){
     $(".menu-toggle").click(function(){
         $(".splash-nav-item").toggle();
     });
+}
+
+function toggleCourses(){
+    $("#splash-nav-courses-toggle").click(function(){
+        $(".splash-nav-courses-list > .splash-nav-dropdown-item").toggle();
+    });
+}
+
+function toggleEducation(){
+    $("#splash-nav-education-toggle").click(function(){
+        $(".splash-nav-education-list > .splash-nav-dropdown-item").toggle();
+    });
+}
+
+$(document).ready(function() {
+    main();
 });
